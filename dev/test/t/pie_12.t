c522649fbe6c14e7f010e95cf913f30e600e72b0
Lichtkind <lichtkind@cpan.org>
2022-06-10 22:26:48 +0200

    reorganise a bit, prepare next release work

--- dev/test/t/pie_12.t
+++ dev/test/t/pie_12.t
@@ -7,7 +7,7 @@ use strict;
 
 print "1..1\n";
 
-my $g = Chart::Pie->new( 500, 450 );
+my $g = Chart::Pie->new( 700, 1450 );
 $g->add_dataset( 'eins', 'zwei', 'drei', 'vier', 'fuenf', 'sechs', 'sieben', 'acht', 'neun', 'zehn', 'elf', 'zwoelf', 13, 14, 15, 16 );
 $g->add_dataset( 20,     20,      12,     27,     19,       28,      19,       23,     15,      60     , 20,     24,    21, 18, 13, 31 );
 
--- lib/Chart/Pie.pm
+++ lib/Chart/Pie.pm
@@ -168,6 +168,17 @@ sub _draw_data
 
     my $labeldistance = 2 * $self->maximum( $fontW, $fontH );
 
+-    my $labeldistance = 2*$self->maximum($fontW,$fontH);
+
+
+-    $diameter = $self->minimum($width,$height) - 2*$max_label_len - $labeldistance;
+
+    my $min_width = $width - 2 * $max_label_len - 2 * $fontW;
+    my $min_height = $height - 2 * $fontH;
++    $diameter = $self->minimum($min_width, $min_height);
+
++    my $labeldistance = 2 * $self->maximum($fontW,$fontH);
+
     $start_degrees = 0;
     $end_degrees   = 0;
     my $max_radius = $self->minimum( $width, $height ) / 2;
@@ -354,7 +365,6 @@ sub _draw_data
 
     }
     $diameter = $radius * 2 - 2 * $labeldistance;
-
     $text_diameter = $diameter + $labeldistance;
     $self->{'gd_obj'}->arc( $centerX, $centerY, $diameter, $diameter, 0, 360, $misccolor );
 
