0c1892d2eaa63d719e584f4183c74321745beb4b
Lichtkind <lichtkind@cpan.org>
2022-06-13 18:44:30 +0200

    complete tt fornt proto

--- dev/function/font/truetype/draw.pl
+++ dev/function/font/truetype/draw.pl
@@ -1,60 +1,58 @@
 use v5.18;
 use GD;
+use Cwd;
 
 my $png_file = 'cavas.png';
+
+my $font_file = './font/unifont.fnt';
+my $agenda    = './font/AGENDA-L.ttf';
+my $bkl       = './font/BKL_CE.ttf';
+my $gothic    = './font/CenturyGothic.ttf';
+my $copper    = './font/CopperplateCondensedSSiCondensed.ttf';
+my $futurama  = './font/FUTURAM.ttf';
+
 
 my $im = GD::Image->new(200,200);
 my $white = $im->colorAllocate( 255, 255, 255);
 my $black = $im->colorAllocate(   0,   0,   0);
 my $blue  = $im->colorAllocate(  40,  40, 160);
 
-$im->string( GD::Font->Tiny ,       20,  20, 'Tiny',       2);
-$im->string( GD::Font->Small ,      20,  50, 'Small',      1);
-$im->string( GD::Font->MediumBold , 20,  80, 'MediumBold', 2);
-$im->string( GD::Font->Large ,      20, 110, 'Large',      1);
-$im->string( GD::Font->Giant ,      20, 140, 'Giant',      2);
+
 
 say "GD Version ".GD::VERSION_STRING." in Module $GD::VERSION \n";
 say "Image object: $im with colors: ", $im->colorsTotal;
-say "  index of first color: ", $im->colorExact( 255, 255, 255);
-say "  index of last color: ",  $im->colorClosest( 50, 50, 150);
-say "  index of no color: ",    $im->colorExact( 50, 50, 150);
-say GD::Font->Small->nchars;
-say GD::Font->Small->offset;
 
+#$ENV{GDFONTPATH} = getcwd;
+#say "fontpath: ",$ENV{GDFONTPATH};
+#say $font_file,' exists: ', -e $font_file;
+#my $unifont =  GD::Font->load( $font_file ) or die "Can't load font";
+#say $unifont;
+
+my @bounds = $im->stringFT(2, $agenda,   11, 0, 30,  30,   'agenda');
+             $im->stringFT(2, $bkl,      11, 0, 30,  50,   'bkl');
+             $im->stringFT(2, $gothic,   11, 0, 30,  70,   'gothic');
+             $im->stringFT(2, $copper,   11, 0, 30,  90,   'copper');
+             $im->stringFT(2, $futurama, 11, 0, 30, 110,   'futurama');
+
+my ($x1, $y1, $x2, $y2, $x3, $y3, $x4, $y4) = @bounds;
+
+#say $agenda if -r $font_file;
 
 open my $FH, '>', $png_file;
 binmode $FH;
-print $FH $im->png( 1);
-#print $FH $im->png();
-#close $FH;
+print $FH $im->png( 0 );
+close $FH;
 
-#$index = $image->getPixel(x,y) 
-#($red,$green,$blue) = $image->rgb($index)
-#($alpha) = $image->alpha($index)
-#$image->transparent($colorIndex)
-
-#$colorsTotal = $image->colorsTotal object method
-# $index = $image->colorAllocate(red,green,blue)
-# $index = $image->colorAllocateAlpha(reg,green,blue,alpha)
-# $image->colorDeallocate(colorIndex)
-# $index = $image->colorClosest(red,green,blue)
-# $index = $image->colorClosestAlpha(red,green,blue,alpha)
-# # $index = $image->colorExact(red,green,blue)
 
 __END__
+ 
+ stringFTCircle($cx,$cy,$radius,$textRadius,$fillPortion,$font,$points,$top,$bottom,$fgcolor)
+ $colorsTotal = $image->colorsTotal object method
+ ($w,$h) = (gdLargeFont->width,gdLargeFont->height);
 
-    
-$font->nchars       This returns the number of characters in the font.
-$font->offset       This returns the ASCII value of the first character in the font
-
-    ($w,$h) = (gdLargeFont->width,gdLargeFont->height);
+@bounds = $image->stringFT($fgcolor,$fontname,$ptsize,$angle,$x,$y,$string,[\%options])
 
 
-$font = GD::Font->load($fontfilepath)
+File::ShareDir
 
-@bounds = $image->stringFT($fgcolor,$fontname,$ptsize,$angle,$x,$y,$string)
-@bounds = GD::Image->stringFT($fgcolor,$fontname,$ptsize,$angle,$x,$y,$string)
-@bounds = $image->stringFT($fgcolor,$fontname,$ptsize,$angle,$x,$y,$string,\%options)
-
-$ENV{GDFONTPATH}
+fnt are different, older format than fft
